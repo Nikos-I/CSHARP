44b8cbd4bf2639a959cce9121401b647b6fc3a2a
Nikos I <nik.norht@gmail.com>
2022-08-10 16:00:26 +0300

    Семинар 2 ДЗ завершено

---
+++ Seminar2/HomeWork2/task13/Program.cs
@@ -0,0 +1,23 @@
+﻿//  Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+    string[] errmsg = {"Нет третьей цифры"};
+    int val1 = 0;
+    int retval = 0;
+    int SecNumber(int val2)
+        {
+            val2 = Math.Abs(val2);
+            if ((val2 < 100) & (val2 != 0) ) { return -1;}
+            
+            return (val2 / 100) % 10;
+        }
+
+    do 
+    {
+        Console.Write("Введите число (0 - завершить работу): ");
+        val1 = Convert.ToInt32(Console.ReadLine());    
+        retval = SecNumber(val1);   
+
+        if (retval > 0) { Console.WriteLine($"{val1} -> {retval}"); }
+        else if (retval < 0) { Console.WriteLine(errmsg[Math.Abs(retval) - 1]); }
+    }
+    while (val1 != 0);
---
+++ Seminar2/HomeWork2/task15/Program.cs
@@ -0,0 +1,13 @@
+﻿// Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+
+    string[] weekend = {"нет", "нет", "нет", "нет", "нет", "да", "да"};
+    int val1 = 0;
+    do 
+    {
+        Console.Write("Введите номер дня недели (0 - завершить работу): ");
+        val1 = Convert.ToInt32(Console.ReadLine());    
+        
+        if ((val1 > 0) & (val1 <= 7) ) { Console.WriteLine($"{val1} -> {weekend[val1-1]}"); }
+        else if (val1 != 0) { Console.WriteLine("Ошибка"); }
+    }
+    while (val1 != 0);
