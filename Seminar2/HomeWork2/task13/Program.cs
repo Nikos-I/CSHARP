c2b6fc2f47bc9b3bef5aab01f4de26f2f405a489
Nikos I <nik.norht@gmail.com>
2022-08-12 15:26:54 +0300

    Работа над ошибками 2 обработка ввода пустого значения

--- Seminar2/HomeWork2/task13/Program.cs
+++ Seminar2/HomeWork2/task13/Program.cs
@@ -1,8 +1,10 @@
 ﻿//  Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
 
 string[] errmsg = {"Нет третьей цифры"};
-int val1 = 0;
+int val1 = -1;
 int retval = 0;
+var valstr = (string?)null;
+
 int ThirdNumber(int val2)
     {
         val2 = Math.Abs(val2);
@@ -14,8 +16,11 @@ int ThirdNumber(int val2)
 do 
 {
     Console.Write("Введите число (0 - завершить работу): ");
-    val1 = Convert.ToInt32(Console.ReadLine());    
+    valstr = Console.ReadLine();
+    if (String.IsNullOrEmpty(valstr)) continue;
+    val1 = Convert.ToInt32(valstr);    
     if (val1 == 0) break;
+    
 
     retval = ThirdNumber(val1);   
     if (retval > 0) Console.WriteLine($"{val1} -> {retval}"); 
--- Seminar2/HomeWork2/task15/Program.cs
+++ Seminar2/HomeWork2/task15/Program.cs
@@ -1,13 +1,17 @@
 ﻿// Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
 
 string[] weekend = {"нет", "нет", "нет", "нет", "нет", "да", "да"};
-int val1 = 0;
+int val1 = -1;
+var valstr = (string?)null;
 do 
 {
 Console.Write("Введите номер дня недели (0 - завершить работу): ");
-    val1 = Convert.ToInt32(Console.ReadLine());    
-            
-    if ((val1 > 0) && (val1 <= 7) ) Console.WriteLine($"{val1} -> {weekend[val1-1]}"); 
-    else if (val1 != 0) { Console.WriteLine("Ошибка"); }
+valstr = Console.ReadLine();
+if (String.IsNullOrEmpty(valstr)) continue;
+val1 = Convert.ToInt32(valstr);    
+if (val1 == 0) break;
+                
+if ((val1 > 0) && (val1 <= 7) ) Console.WriteLine($"{val1} -> {weekend[val1-1]}"); 
+else if (val1 != 0) { Console.WriteLine("Ошибка"); }
 }
 while (val1 != 0);
