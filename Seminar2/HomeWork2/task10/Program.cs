55e3389f6bc22d445710181d6b2d56cf82edf61a
Nikos I <nik.norht@gmail.com>
2022-08-12 14:56:07 +0300

    Работа над ошибками

--- Seminar2/HomeWork2/task10/Program.cs
+++ Seminar2/HomeWork2/task10/Program.cs
@@ -1,21 +1,21 @@
 ﻿//  Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
 
-    string[] errmsg = {"Значение меньше 100","Значение больше 999"};
-    int val1 = 0;
-    int retval = 0;
-    int SecNumber(int val2)
-        {
-            if (val2 < 100) { return -1;}
-            else if (val2 > 999) { return -2;}
-            
-            return (val2 / 10) % 10;
-        }
+string[] errmsg = {"Значение меньше 100","Значение больше 999"};
+int val1 = 0;
+int retval = 0;
+int SecNumber(int val2)
+{
+    if (val2 < 100) return -1; 
+    else if (val2 > 999) return -2;
+    
+    return (val2 / 10) % 10;
+}
 
-    Console.Write("Введите трехзначное число: ");
-    val1 = Convert.ToInt32(Console.ReadLine());    
+Console.Write("Введите трехзначное число: ");
+val1 = Convert.ToInt32(Console.ReadLine());
 
-    retval = SecNumber(val1);   
+retval = SecNumber(val1);   
 
-    if (retval >= 0) { Console.WriteLine($"{val1} -> {retval}"); }
-    else { Console.WriteLine(errmsg[Math.Abs(retval) - 1]); }
+if (retval >= 0) Console.WriteLine($"{val1} -> {retval}"); 
+else Console.WriteLine(errmsg[Math.Abs(retval) - 1]); 
 
--- Seminar2/HomeWork2/task13/Program.cs
+++ Seminar2/HomeWork2/task13/Program.cs
@@ -1,23 +1,24 @@
 ﻿//  Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
 
-    string[] errmsg = {"Нет третьей цифры"};
-    int val1 = 0;
-    int retval = 0;
-    int ThirdNumber(int val2)
-        {
-            val2 = Math.Abs(val2);
-            if ((val2 < 100) & (val2 != 0) ) { return -1;}
-            
-            return (val2 / 100) % 10;
-        }
-
-    do 
+string[] errmsg = {"Нет третьей цифры"};
+int val1 = 0;
+int retval = 0;
+int ThirdNumber(int val2)
     {
-        Console.Write("Введите число (0 - завершить работу): ");
-        val1 = Convert.ToInt32(Console.ReadLine());    
-        retval = ThirdNumber(val1);   
+        val2 = Math.Abs(val2);
+        if ((val2 < 100))  return -1;
 
-        if (retval > 0) { Console.WriteLine($"{val1} -> {retval}"); }
-        else if (retval < 0) { Console.WriteLine(errmsg[Math.Abs(retval) - 1]); }
+        return (val2 / 100) % 10;
     }
-    while (val1 != 0);
+
+do 
+{
+    Console.Write("Введите число (0 - завершить работу): ");
+    val1 = Convert.ToInt32(Console.ReadLine());    
+    if (val1 == 0) break;
+
+    retval = ThirdNumber(val1);   
+    if (retval > 0) Console.WriteLine($"{val1} -> {retval}"); 
+    else if (retval < 0) Console.WriteLine(errmsg[Math.Abs(retval) - 1]); 
+}
+while (val1 != 0);
