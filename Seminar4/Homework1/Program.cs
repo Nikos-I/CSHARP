f75d7a03636ec676465a663b8fa29c3905e05bd0
Nikos I <nik.norht@gmail.com>
2022-08-16 17:58:32 +0300

    Семинар 4 ДЗ решены две задачи

---
+++ Seminar4/Homework1/Program.cs
@@ -0,0 +1,37 @@
+﻿// Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+// 3, 5 -> 243 (3⁵)
+ //2, 4 -> 16
+
+
+int ReadInt() // Ввод целого числа. 0 - особый случай
+{
+    int val1;
+    var valstr = (string?)null;
+    do
+    {
+        
+        valstr = Console.ReadLine();
+        if (!(String.IsNullOrEmpty(valstr)) && (int.TryParse(valstr, out val1))) return val1;   
+         
+        Console.WriteLine("Ошибка ввода");
+    }
+    while (true);
+}
+ int A, B, Result =  -1;
+ 
+do
+{
+    Console.Write($"Введите положительное целое число A (0 - завершить работу): ");
+    A = ReadInt();
+    if (A == 0) continue;
+    Console.Write($"Введите положительное целое число B (степень): ");
+    B = ReadInt();
+
+    for (int i=1; i <= B; i++)
+    {
+        if (i == 1) Result = A;
+
+        else Result = Result * A;
+    }
+    if (A > 0) Console.WriteLine($"{A},{B} -> {Result}({A}^{B})");
+} while (A != 0);
---
+++ Seminar4/Homework2/Program.cs
@@ -0,0 +1,38 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+int ReadInt() // Ввод целого числа. 0 - особый случай
+{
+    int val1;
+    var valstr = (string?)null;
+    do
+    {
+        
+        valstr = Console.ReadLine();
+        if (!(String.IsNullOrEmpty(valstr)) && (int.TryParse(valstr, out val1))) return val1;   
+         
+        Console.WriteLine("Ошибка ввода");
+    }
+    while (true);
+}
+    int val1 =  -1;
+    string result = "";
+    int sum = 0;
+ 
+do
+{
+
+    Console.Write($"Введите положительное целое число (0 - завершить работу): ");
+    val1 = ReadInt();
+    if (val1 == 0) continue;
+    
+    result = "";
+    sum = 0;
+    result = val1.ToString();
+
+    foreach(char c in result) sum = sum + Convert.ToInt32(c.ToString());
+    Console.WriteLine($"{val1} -> {sum} ");
+   
+} while (val1 != 0);
