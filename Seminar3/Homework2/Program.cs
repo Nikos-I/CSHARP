10e9560c701df7ae838542b9e007de9eecb2b93b
Nikos I <nik.norht@gmail.com>
2022-08-16 16:19:57 +0300

    Семинар 3 ДЗ завершено полностью

---
+++ Seminar3/Homework2/Program.cs
@@ -0,0 +1,43 @@
+﻿// Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+
+int ReadInt() // Ввод целого числа. 
+    {
+        int val1;
+        var valstr = (string?)null;
+        do
+        {
+            valstr = Console.ReadLine();
+            if (!(String.IsNullOrEmpty(valstr)) && (int.TryParse(valstr, out val1))) return val1;   
+            Console.Write("Ошибка ввода");
+        }
+        while (true);
+    }
+void CoordPoint(string n_point, out int x, out int y, out int z) // Ввод координат точки n_point
+    {
+        Console.WriteLine($"Введите координаты точки {n_point}");
+        Console.Write("Х=");
+        x = ReadInt();
+        Console.Write("Y=");
+        y = ReadInt();
+        Console.Write("Z=");
+        z = ReadInt();
+    }
+
+int Ax, Ay, Az = 0;
+int Bx, By, Bz = 0;
+
+CoordPoint("A", out Ax, out Ay, out Az);
+CoordPoint("B", out Bx, out By, out Bz);
+
+double dist = 0;
+dist = Math.Abs(Math.Sqrt(Math.Pow(Bx - Ax, 2) + Math.Pow(By - Ay, 2) + Math.Pow(Bz - Az, 2)));
+
+Console.WriteLine($"A({Ax},{Ay},{Az}); B({Bx},{By},{Bz}) -> {Math.Round(dist, 2)}");
+
+
+
+
+
+
+
+
---
+++ Seminar3/Homework3/Program.cs
@@ -0,0 +1,29 @@
+﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+int ReadInt() // Ввод целого числа. 0 - особый случай
+{
+    int val1;
+    var valstr = (string?)null;
+    do
+    {
+        Console.Write($"Введите положительное целое число (0 - завершить работу): ");
+        valstr = Console.ReadLine();
+        if (!(String.IsNullOrEmpty(valstr)) && (int.TryParse(valstr, out val1))) return val1;   
+         
+        Console.WriteLine("Ошибка ввода");
+    }
+    while (true);
+}
+string  result = "";
+ int n =  -1;
+
+do
+{
+    n = ReadInt();
+    for (int i=1; i <= n; i++)
+    {
+        if (i == 1) result = Convert.ToString(Math.Pow(i, 3));
+        else result = result + "," + Convert.ToString(Math.Pow(i, 3));
+    }
+    if (n > 0) Console.WriteLine($"{n} -> {result}");
+} while (n != 0);
