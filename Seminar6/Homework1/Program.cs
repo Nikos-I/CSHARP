bd96fe3e01cbeaf0e410e67577e56848e57449f9
Nikos I <nik.norht@gmail.com>
2022-08-23 13:18:23 +0300

    Семинар 6 ДЗ Завершено

--- Seminar6/Homework1/Program.cs
+++ Seminar6/Homework1/Program.cs
@@ -1,2 +1,36 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+
+int ReadInt() // Ввод целого числа..
+{
+    int val1;
+    var valstr = (string?)null;
+    do
+    {
+        
+        valstr = Console.ReadLine();
+        if (!(String.IsNullOrEmpty(valstr)) && (int.TryParse(valstr, out val1))) return val1;   
+         
+        Console.WriteLine("Ошибка ввода");
+    }
+    while (true);
+}
+
+int M = 5;
+int count_pos = 0;
+int[] result = new int[M];
+
+Console.WriteLine($"Программа вводит с клавиатуры M={M} чисел. Выводит количество чисел больше 0.");    
+
+for (int i = 0; i < M; i++)
+{
+    result[i] = ReadInt();
+    if (result[i] > 0) count_pos += 1;
+}
+System.Console.Write("[");
+for (int i = 0; i <result.Length; i++)
+{
+    System.Console.Write($"{result[i]}, ");
+}
+System.Console.Write($"\b\b] -> {count_pos}");
---
+++ Seminar6/Homework2/Program.cs
@@ -0,0 +1,36 @@
+﻿// Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+int ReadInt() // Ввод целого числа. 0 - особый случай
+{
+    int val1;
+    var valstr = (string?)null;
+    do
+    {
+        
+        valstr = Console.ReadLine();
+        if (!(String.IsNullOrEmpty(valstr)) && (int.TryParse(valstr, out val1))) return val1;   
+         
+        Console.WriteLine("Ошибка ввода");
+    }
+    while (true);
+}
+
+Console.WriteLine($"Программа находит точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; \nзначения b1, k1, b2 и k2 задаются пользователем.");    
+
+System.Console.Write($"b1:");
+double b1 = Convert.ToDouble(ReadInt());
+System.Console.Write($"k1:");
+double k1 = Convert.ToDouble(ReadInt());
+System.Console.Write($"b1:");
+double b2 = Convert.ToDouble(ReadInt());
+System.Console.Write($"k2:");
+double k2 = Convert.ToDouble(ReadInt());
+
+double x, y = 0;
+//  b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+// y = k1 * x + b1 - y = k2 * x + b2 => 0 =  k1 * x + b1 -k2*x - b2 => (k1-k2)*x  = -(b1-b2)
+x = -(b1 - b2) / (k1-k2);
+y = k2*x + b2;
+
+System.Console.Write($"[b1 = {b1}, k1 = {k1}, b2 = {b2}, k2 = {k2}] -> ({x}; {y})");
